1876e291fe7d8a1c701d17e69300aa68b0a99874
mlanzamolina <39623745+mlanzamolina@users.noreply.github.com>
2023-08-16 22:16:14 +0000

    Deploying to gh-pages from @ mlanzamolina/caja-chica@b4dad51c4935e875486cb3d56a35ccf8be4f8d09 🚀

---
+++ env.js
@@ -0,0 +1,26 @@
+window.env = {
+  REACT_APP_NAME: "telehuertos",
+  REACT_APP_PROJECTS: [
+    {
+      id: 1,
+      name: "caja-chica",
+      description: "Descripción de caja-chica",
+      db: "todo",
+      collection: "Item",
+    },
+    {
+      id: 2,
+      name: "telehuertos",
+      description: "Descripción de telehuertos",
+      db: "Telehuertos",
+      collection: "contenido",
+    },
+    {
+      id: 3,
+      name: "tele-ejemplo",
+      description: "Descripción del tele-ejemplo",
+      db: "db-ejemplo",
+      collection: "collection-ejemplo",
+    },
+  ],
+};
